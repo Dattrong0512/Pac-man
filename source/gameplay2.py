69ccabfce8736329b8c35194942370e83645242b
DatTrong <trongleviet05@gmail.com>
2024-04-14 18:49:36 +0700

    Commit total

---
+++ source/gameplay2.py
@@ -0,0 +1,161 @@
+import heapq
+import extract
+from pygame import event, QUIT, display
+import render
+
+# Heuristic function (h(x): estimated distance from processing location to goal)
+def heuristic(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+# Get neighbors of a given point
+def get_neighbors(matrix, node): # node = tuple (x,y) => node[0] = x, node[1] = y
+    neighbors = []
+    for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Phải trái trên dưới
+        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : # check index of node is out of range and not a wall
+            if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 3: # treat monster like wall
+                neighbors.append((node[0] + i, node[1] + j))   
+    return neighbors # tuple (x,y)
+
+def get_neighbors_monster(matrix, node): # this function like above but not process monster
+    neighbors = []
+    for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Phải trái trên dưới
+        if 0 <= node[0] + i < len(matrix) and 0 <= node[1] + j < len(matrix[0]) and matrix[node[0] + i][node[1] + j] != 1 : # check index of node is out of range and not a wall
+                neighbors.append((node[0] + i, node[1] + j))   
+    return neighbors
+
+
+
+def astar(matrix, start, goal):
+    open_set = []
+    heapq.heappush(open_set, (0, start))
+    came_from = {start: None}
+    g_score={start: 0}
+    while open_set:
+        current = heapq.heappop(open_set)[1]
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.reverse()
+            return path
+
+        for neighbor in get_neighbors(matrix, current):
+            temp_g = g_score[current] + 1
+            
+            if neighbor not in g_score or temp_g < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g
+                f_score = temp_g + heuristic(neighbor, goal)
+                if neighbor not in open_set:
+                    heapq.heappush(open_set, (f_score, neighbor))
+    return None
+
+
+def astar_monster(matrix, start, goal):
+    open_set = []
+    heapq.heappush(open_set, (0, start))
+    came_from = {start: None}
+    g_score = {start: 0}
+    while open_set:
+        current = heapq.heappop(open_set)[1] 
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.reverse()
+            return path
+        
+        for neighbor in get_neighbors_monster(matrix, current):
+            temp_g = g_score[current] + 1
+            if neighbor not in g_score or temp_g < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g
+                f_score = temp_g + heuristic(neighbor, goal)
+                heapq.heappush(open_set, (f_score, neighbor))
+     
+    return None
+
+
+def find_path_to_food(matrix, pacman_position):
+    food_position = None
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 2:
+                food_position = (i,j)        
+    if food_position is None:
+        return None
+
+    return astar(matrix, pacman_position, food_position)
+
+
+# Update Pacman's position based on the path found by A*
+def update_pacman_position(matrix, pacman_position):
+    path_to_food = find_path_to_food(matrix, pacman_position)
+    food_position = None
+    monster_positions = []
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if matrix[i][j] == 2:
+                food_position = (i, j)
+            if matrix[i][j] == 3:
+                monster_positions.append((i,j))
+ 
+    if path_to_food:
+        next_position = path_to_food[1]  # Lấy vị trí thứ hai trong danh sách đường đi
+        if matrix[next_position[0]][next_position[1]] == 3:  # Kiểm tra nếu Pacman chạm vào quái vật
+            print("Game Over")
+            return (-1,-1)
+        else:
+            matrix[pacman_position[0]][pacman_position[1]] = 999  # Đánh dấu lại vị trí mà Pacman đã đi qua
+            matrix[next_position[0]][next_position[1]] = 888  # Di chuyển Pacman đến vị trí tiếp theo
+            if (next_position[0], next_position[1]) == food_position:
+                print("You Win")
+                return False
+            return next_position
+    else:
+
+        path_to_monster = astar_monster(matrix, pacman_position, food_position) # still the shortest way to food but pacman must collide with monster
+        next_position = path_to_monster[1] if path_to_monster else pacman_position
+
+        matrix[pacman_position[0]][pacman_position[1]] = 999  # Đánh dấu lại vị trí mà Pacman đã đi qua
+        if (next_position[0], next_position[1]) in monster_positions:
+            print("Game Over")
+            return (-1,-1)
+        matrix[next_position[0]][next_position[1]] = 888  # Di chuyển Pacman đến vị trí tiếp theo
+
+        return next_position
+
+def play(timer, screen):
+    fps = 10
+    run = True      
+    matrix = extract.extractMatrix('map{}.txt'.format(2))
+    player_location = extract.extractLocation('map{}.txt'.format(2))      
+    counter = 0
+    score_value = 100           
+    player_x = player_location[0] 
+    player_y = player_location[1]
+    while run:      
+        timer.tick(fps)
+        if counter < 19:
+            counter+=1
+        else:
+            counter = 0                                        
+        screen.fill('black')                                      
+        if  (player_x, player_y) != (-1,-1):
+            player_x, player_y = update_pacman_position(matrix, (player_x, player_y))
+            score_value -= 1
+            if score_value == 0:
+                run = False
+        else:
+            run = False
+
+        render.render(matrix)
+        render.render_score(300,700,score_value)
+
+        for eventElement in event.get():
+            if eventElement.type == QUIT:
+                    run = False   
+        display.flip()     
---
+++ source/main.py
@@ -0,0 +1,192 @@
+import render
+
+import pygame   
+import numpy as np
+import gameplay1
+import gameplay2
+import gameplay3
+import gameplay4
+
+
+pygame.init()
+pygame.font.init()
+WIDTH = 700 # width of console
+HEIGHT = 750 # height of console
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+timer = pygame.time.Clock()
+
+font = pygame.font.Font('source/assets/font/freesansbold.ttf',32)
+
+return_image = pygame.transform.scale(pygame.image.load(f'source/assets/return_images/return_images.png'),(30,30))
+
+creditFont = pygame.font.Font("source/assets/font/freesansbold.ttf", 20)
+
+creditList = [
+    "21120402 - Truong Hoang Nhan",
+    "21120403 - Nguyen Hoang Quan",
+    "21120406 - Le Viet Dat Trong",
+    "21120414 - Ha Quoc Bao"
+]
+ 
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    screen.fill((0,0,0)) # Black color for screen
+    
+    largeText = pygame.font.Font('source/assets/font/freesansbold.ttf', 100)#Tạo phông chữ với kích cỡ 100 pixel
+    TextSurf, TextRect = render.text_objects("Pacman Game", largeText)
+    TextRect.center = ((WIDTH / 2), (45))
+    screen.blit(TextSurf, TextRect)
+
+    # Creating buttons
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    
+
+    # Start button
+    if 290 < mouse[0] < 410 and 120 <mouse[1] <180:# Nếu di chuyển chuột vào vugnf này       
+        pygame.draw.rect(screen, (0,200,0), (290,120,120,60)) 
+        if click[0] == 1:               
+            pygame.mouse.set_pos((289,119))
+            run_Level=True
+            while run_Level:        
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+
+                screen.fill((0,0,0)) # Black color for screen
+
+                # Creating buttons
+                mouse = pygame.mouse.get_pos()
+                click = pygame.mouse.get_pressed()
+
+                # Level 1
+                if 290 < mouse[0] < 410 and 120 <mouse[1] <180:#Nếu di chuyển chuột vào vugnf này
+                    pygame.draw.rect(screen, (0,200,0), (290,120,120,60)) # Brighter when hover
+                    if click[0] == 1:
+                        gameplay1.play(timer, screen)
+                                                                             
+                else:
+                    pygame.draw.rect(screen, (0,255,0), (290,120,120,60)) # Darker normally
+                    
+                smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 20)
+                TextSurf, TextRect = render.text_objects("Level 1", smallText)
+                TextRect.center = ((350), (150))#Vị trí chữ
+                screen.blit(TextSurf, TextRect)
+                                        
+                # Level 2
+                if 290 < mouse[0] < 410 and 200 <mouse[1] <260:#Nếu di chuyển chuột vào vùng này
+                    pygame.draw.rect(screen, (255,250,0), (290,200,120,60)) # Brighter when hover
+                    if click[0] == 1:
+                        gameplay2.play(timer, screen)
+                                             
+                else:
+                    pygame.draw.rect(screen, (255,150,0), (290,200,120,60)) # Darker normally
+            
+                smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 20)
+                TextSurf, TextRect = render.text_objects("Level 2", smallText)
+                TextRect.center = ((350), (230))#Vị trí chữ
+                screen.blit(TextSurf, TextRect)
+                            
+                # Level 3
+                if 290 < mouse[0] < 410  and 280 <mouse[1] <340:
+                    pygame.draw.rect(screen, (200,0,0), (290,280,120,60)) # Brighter when hover
+                    if click[0] == 1:
+                        gameplay3.play(timer, screen)
+                        
+                else:
+                    pygame.draw.rect(screen, (255,0,0), (290,280,120,60)) # Darker normally
+
+                smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 19)
+                TextSurf, TextRect = render.text_objects("Level 3", smallText)
+                TextRect.center = (350,310)
+                screen.blit(TextSurf, TextRect)                
+                
+                # Level 4
+                if 290 < mouse[0] < 410  and 360 <mouse[1] <420:
+                    pygame.draw.rect(screen, (128, 0, 128), (290,360,120,60)) # Brighter when hover
+                    if click[0] == 1:
+                        gameplay4.play(timer,screen)
+                        
+                else:
+                    pygame.draw.rect(screen, (139, 0, 139), (290,360,120,60)) # Darker normally
+
+                smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 19)
+                TextSurf, TextRect = render.text_objects("Level 4", smallText)
+                TextRect.center = (350,390)
+                screen.blit(TextSurf, TextRect)
+                
+                
+                if 30 < mouse[0] < 60  and 30 <mouse[1] <60:
+                    screen.blit(return_image, (30, 30)) 
+                    pygame.draw.rect(screen, (255,255,255), (30,30,30,30)) # Brighter when hover
+                    if click[0] == 1:
+                        run_Level=False                       
+                else:
+                    pygame.draw.rect(screen, (50, 50, 50), (30,30,30,30)) # Darker normally                    
+               
+                screen.blit(return_image, (30, 30))    
+                pygame.display.flip()
+    else:
+        pygame.draw.rect(screen, (0,255,0), (290,120,120,60))   
+                                                                                        
+    smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 20)
+    TextSurf, TextRect = render.text_objects("Start Game", smallText)
+    TextRect.center = ((350), (150))#Vị trí chữ
+    screen.blit(TextSurf, TextRect)
+        
+
+    # Credit Button
+    if 290 < mouse[0] < 410 and 200 <mouse[1] <260:#Nếu di chuyển chuột vào vugnf này
+        pygame.draw.rect(screen, (255,250,0), (290,200,120,60)) # Brighter when hover
+        if click[0] == 1:
+            show_Credit=True
+            while show_Credit:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                screen.fill((0,0,0)) # Black color for screen
+                for i, line in enumerate(creditList):
+                    lineSurface, lineRect = render.text_objects(line, creditFont)
+                    lineRect.center = (WIDTH / 2, 200 + 30*i) # Adjust as necessary
+                    screen.blit(lineSurface, lineRect)
+                mouse = pygame.mouse.get_pos()    # Update mouse position
+                click = pygame.mouse.get_pressed()  # Update mouse clicks
+                if 30 < mouse[0] < 60  and 30 <mouse[1] <60:
+                    screen.blit(return_image, (30, 30)) 
+                    pygame.draw.rect(screen, (255,255,255), (30,30,30,30)) # Brighter when hover
+                    if click[0] == 1:
+                        show_Credit=False                       
+                else:
+                    pygame.draw.rect(screen, (50, 50, 50), (30,30,30,30)) # Darker normally                    
+                screen.blit(return_image, (30, 30))    
+                pygame.display.update()
+    else:
+        pygame.draw.rect(screen, (255,150,0), (290,200,120,60)) # Darker normally
+
+    smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 20)
+    TextSurf, TextRect = render.text_objects("Credit", smallText)
+    TextRect.center = ((350), (230))#Vị trí chữ
+    screen.blit(TextSurf, TextRect)
+    
+            
+    # Quit button
+    if 290 < mouse[0] < 410  and 280 <mouse[1] <340:
+        pygame.draw.rect(screen, (200,0,0), (290,280,120,60)) # Brighter when hover
+        if click[0] == 1:
+            pygame.quit()
+            quit()
+    else:
+        pygame.draw.rect(screen, (255,0,0), (290,280,120,60)) # Darker normally
+
+    smallText = pygame.font.Font("source/assets/font/freesansbold.ttf", 19)
+    TextSurf, TextRect = render.text_objects("Quit Game", smallText)
+    TextRect.center = (350,310)
+    screen.blit(TextSurf, TextRect)
+    pygame.display.flip()
